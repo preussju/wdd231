6a9605f891a57debf052d1b33b14f6e7a978c1dd
preussju <jveiga@byupathway.edu>
2025-09-02 21:47:08 -0300

    learning activities

---
+++ week01/site-header/scripts/navigation.js
@@ -0,0 +1,9 @@
+const navButton = document.querySelector('#ham-btn');
+const navLinks = document.querySelector('#nav-bar');
+
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navLinks.classList.toggle('show');
+});
+
