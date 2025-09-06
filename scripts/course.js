011cd4f9d4520c5f8019e59bf14f4962972ba84b
preussju <jveiga@byupathway.edu>
2025-09-06 00:16:43 -0300

    js almost done

--- scripts/course.js
+++ scripts/course.js
@@ -1,9 +1,85 @@
 
 const courses = [
-    { course: "CSPC105", type: "cse", credits: 3 }, { course: "CSE111", type: "cse", credits: 2 },
-    { course: "CSE210", type: "cse", credits: 2 }, { course: "WDD130", type: "wdd", credits: 2 },
-    { course: "WDD131", type: "wdd", credits: 2 }, { course: "WDD231", type: "wdd", credits: 2 }
+    {
+        subject: 'CSPC',
+        number: 105,
+        title: 'Introduction to Programming',
+        credits: 3,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+            'HTML',
+            'CSS'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        technology: [
+            'Python'
+        ],
+        completed: true
+    },
+    {
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+        technology: [
+            'C#'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: true
+    },
+    {
+        subject: 'WDD',
+        number: 231,
+        title: 'Frontend Web Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    }
 ]
+
+
 
 createCourseCards(courses);
 
@@ -13,7 +89,7 @@ function createCourseCards(filteredCourses) {
         let card = document.createElement("section")
         let content = document.createElement("p")
 
-        content.textContent = course.course
+        content.textContent = course.subject + course.number
         card.append(content);
         document.querySelector(".container").appendChild(card);
     })
@@ -27,8 +103,8 @@ const wdd = document.querySelector("#wdd");
 all.addEventListener("click", () => {
     createCourseCards(courses)
     let sum = 0;
-    cour = courses.filter(course => course.type == "cse" || course.type == "wdd");
-    cour.forEach(course => {
+    sub = courses.filter(course => course.subject == "CSE" || course.subject == "WDD" || course.subject == "CSPC");
+    sub.forEach(course => {
         sum += course.credits;
         const creditsElement = document.getElementById("credits");
         creditsElement.textContent = `${sum}`;
@@ -36,9 +112,9 @@ all.addEventListener("click", () => {
 })
 
 cse.addEventListener("click", () => {
-    createCourseCards(courses.filter(course => course.type == "cse"));
+    createCourseCards(courses.filter(course => course.subject == "CSE" || course.subject == "CSPC"));
     let sum = 0;
-    cseCourses = courses.filter(course => course.type == "cse");
+    cseCourses = courses.filter(course => course.subject == "CSE" || course.subject == "CSPC");
     cseCourses.forEach(course => {
         sum += course.credits;
         const creditsElement = document.getElementById("credits");
@@ -48,9 +124,9 @@ cse.addEventListener("click", () => {
 });
 
 wdd.addEventListener("click", () => {
-    createCourseCards(courses.filter(course => course.type == "wdd"));
+    createCourseCards(courses.filter(course => course.subject == "WDD"));
     let sum = 0;
-    wddCourses = courses.filter(course => course.type == "wdd");
+    wddCourses = courses.filter(course => course.subject == "WDD");
     wddCourses.forEach(course => {
         sum += course.credits;
         const creditsElement = document.getElementById("credits");
